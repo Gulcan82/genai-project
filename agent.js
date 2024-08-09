8ec158e8fb797eb69c15e8da6106515b91a3eeab
Gülcan Kamat <gulcankamat@gmail.com>
2024-08-09 12:07:48 +0200

    Modifying the agent output to stream.

--- agent.js
+++ agent.js
@@ -7,25 +7,30 @@ const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
 export class Agent {
     constructor(personality) {
         this.personality = personality;
-        this.observations = ["Es ist ein schöner Tag heute"];
+        this.observations = ["Es ist ein schöner Tag heute."];
         this.actions = ["Ich habe gerade einen leckeren Kaffee getrunken."];
         this.locations = [];
         this.planOfDay = "";
-
     }
 
     async simulateObservation(currentTime) {
-        const completion = await openai.chat.completions.create({
-            messages: [{ role: "system", content: this. personality },
-                { role: "user", content: `Stelle dir vor, du bist diese Person: ${this.personality}. Es ist ${currentTime} jetzt. Deine letzten Beobachtungen sind ${this.observations}. Deine letzten Handlungen sind ${this.actions}. Basierend darauf, was gerade passiert, was du gerade gemacht hast und deinen letzten Beobachtungen, beschreibe deine Beobachtungen zu diesem Zeitpunkt. `}
-            ],
 
+        const messages = [{ role: "system", content: this.personality },
+            { role: "user", content: `Stelle dir vor, du bist diese Person: ${this.personality}. Es ist ${currentTime} jetzt. Deine letzten Beobachtungen sind ${this.observations}. Deine letzten Handlungen sind ${this.actions}. Basierend darauf, was gerade passiert, was du gerade gemacht hast und deinen letzten Beobachtungen, beschreibe deine Beobachtungen zu diesem Zeitpunkt. Sei kurz, schreibe maximal 2 Sätze.` }
+        ];
+
+        const responseStream = await openai.chat.completions.create({
+            messages,
             model: "gpt-3.5-turbo",
+            stream: true
           });
         
-          console.log(completion.choices[0]);
-
+        for await (const part of responseStream) {
+            const delta = part.choices[0].delta;
+            if (delta && delta.content) {
+                process.stdout.write(delta.content);
+            }
+        }
     }
-
 
 }
